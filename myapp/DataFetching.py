5176e9045f60e39d8835d08bf7e195449e0f42f6
muhammedimad <Muhammedshadi@gmail.com>
2019-10-21 20:02:40 +0300

    Modifications on my branch

--- myapp/DataFetching.py
+++ myapp/DataFetching.py
@@ -1,45 +1,50 @@
 import requests
 import sqlite3
-import sys
 
 
 def connect():
-    database = sqlite3.connect('testDatabase.db',isolation_level=None,timeout=10)
+    database = sqlite3.connect('testDatabase.db', isolation_level=None, timeout=10)
     return database
-stocksDatabase=connect()
+
+
+stocksDatabase = connect()
+
 
 def fetch_stock(symbol):
-    url = "https://sandbox.iexapis.com/stable/stock/"+str(symbol)+"/quote?token=Tpk_087a52e2547e449e9cee8748f00491eb"
-    response=requests.get(url)
-    if (response.status_code)==404:
+    url = "https://sandbox.iexapis.com/stable/stock/" + str(
+        symbol) + "/quote?token=Tpk_087a52e2547e449e9cee8748f00491eb"
+    response = requests.get(url)
+    if (response.status_code) == 404:
         return None
     else:
         dataset = response.json()
+        print(dataset)
     return (dataset)
+
 
 def is_cached(ssymbol):
     c1 = stocksDatabase.cursor()
-
     res = list(c1.execute('''select symbol from stocks '''))
-
-    res=flatter(res)
     print(res)
+    res = flatter(res)
+
     if ssymbol in res:
         return True
     else:
         return False
 
+
 def add_stock_to_db(symbol):
     if is_cached(symbol.upper()) is True:
-
-        sys.exit()
+        return None
     else:
-        stock_data=fetch_stock(symbol)
-        if stock_data==0:
+        stock_data = fetch_stock(symbol)
+        if stock_data == 0:
             exit(0)
-        else :
+        else:
             c1 = stocksDatabase.cursor()
             c1.execute("INSERT INTO stocks VALUES (?,?)", [stock_data["symbol"], stock_data["companyName"]])
+
 
 def flatter(tu):
     l = []
@@ -47,5 +52,4 @@ def flatter(tu):
         l.append(i[0])
     return l
 
-
-add_stock_to_db('AMD')
+add_stock_to_db('amd')
