7c26ee1072d21189975aa02615f0b37f1db2fae4
MajdVictor <majdhafiri@gmail.com>
2019-10-21 17:21:15 +0300

    added a comment

--- myapp/tests/test_apis.py
+++ myapp/tests/test_apis.py
@@ -4,8 +4,9 @@ import unittest
 import requests
 
 #Unit test to check if the response.status_code is 200 for the API "most active stocks"
+#Run command: python3 manage.py test myapp.tests
 class APITest(unittest.TestCase):
-    
+
     def setUp(self):
         TestCase.__init__(self)
         self.base_url = 'https://sandbox.iexapis.com/stable/stock/market/list/mostactive?token=Tpk_c818732500c24764801eb121fa658bb6'
